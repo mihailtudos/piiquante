d865da9d5bc9456e442c99dadae2a27ffadfbfa7
mihailtudos <mihailtudos@outlook.com>
2021-09-16 13:15:26 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+//package imports
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+//imports of default node packages
+const path = require('path');
+
+//route imports
+const userRoutes = require('./routes/user');
+
+//running express function to create an express app
+const app = express();
+
+mongoose.connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas', error);
+  })
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+// app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
---
+++ backend/controller/user.js
@@ -0,0 +1,62 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../model/user');
+
+exports.signup = ((req, res, next) => {
+  // hashing the password
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+
+      user.save()
+        .then((user) => {
+          res.status(201).json({
+            message: 'User created successfully'
+          });
+        })
+        .catch((error) => {
+          res.status(400).json({
+            message: 'Something went wrong while trying to log you in, please try again later'
+          })
+        })
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error
+      });
+    });
+});
+
+exports.login = ((req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if(!user) {
+        return res.status(401).json({
+          error: new Error('Account details don\'t match!')
+        })
+      }
+      bcrypt.compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            res.status(401).json({
+              error: new Error('Account details don\'t match')
+            })
+          }
+
+          const token = jwt.sign(
+            { userId: user._id },
+            {},
+            { expiresIn: '24h' }
+          )
+        })
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error
+      })
+    })
+});
